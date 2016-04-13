5c71d54e5434efeeca3d73a5c27e31b4775e6403
Mark Medum Bundgaard <mmedum@gmail.com>
2016-04-13 23:39:01 +0200

    Should be able to split in slices and corrected logic for calculating probability

--- src/probability_of_zero.py
+++ src/probability_of_zero.py
@@ -46,31 +46,59 @@ def main():
     parser.add_argument("-in", dest="input", required=True,
                         help="Vcf file location", metavar="FILE",
                         type=lambda x: is_valid_file(parser, x))
+    parser.add_argument("-out", dest="output", required=True,
+                        help="Output file location", metavar="FILE")
+    parser.add_argument("-s", dest="slice", required=True,
+                        help="Slice for each chromosome",
+                        metavar="VERVET", type=int)
 
     args = parser.parse_args()
-    vcf_reader = vcf.Reader(open(args.input, "r"))
-    # Hold all the sample information
-    samples = vcf_reader.samples
 
-    male = female = 0
-    zero_male_genotype = 0
-    zero_one_female_genotype = zero_zero_female_genotype = 0
+    with open(args.output, 'a') as out:
+        out.write("Start\tEnd\tProbability\n")
+        vcf_reader = vcf.Reader(open(args.input, "r"))
+        # Hold all the sample information
+        samples = vcf_reader.samples
 
-    for record in vcf_reader:
-        gender = "none"
-        genotype = "-1/-1"
-        for sample in samples:
-            genotype = record.genotype(sample)["GT"].lower().strip()
-            match_male_female = re.search(r"[\w]*_(\w)_[\d]+", sample)
-            if match_male_female:
-                gender = match_male_female.group(1).lower().strip()
-            else:
-                print("Not possible to find gender, aborting program")
-                sys.exit(1)
-            male, female = accumulate_gender(male, female, gender, genotype)
-            zero_male_genotype, zero_one_female_genotype, zero_zero_female_genotype = accumulate_genotypes(zero_male_genotype, zero_one_female_genotype, zero_zero_female_genotype, genotype, gender)
+        window_slice = args.slice
+        should_set_start_pos = True
+        start_pos = -1
 
-    print(zero_male_genotype, zero_one_female_genotype, zero_zero_female_genotype, male, female)
+        male = female = 0
+        zero_male_genotype = 0
+        zero_one_female_genotype = zero_zero_female_genotype = 0
+
+        for record in vcf_reader:
+            if should_set_start_pos:
+                start_pos = record.POS
+                out.write(str(start_pos))
+                out.write("\t")
+                should_set_start_pos = False
+            if (record.POS - start_pos) >= window_slice:
+                end_position = record.POS
+                out.write(str(end_position))
+                out.write("\t")
+                numerator = zero_male_genotype + zero_one_female_genotype + (2 * zero_zero_female_genotype)
+                denominator = male + (2 * female)
+                out.write(str(numerator/denominator))
+                out.write("\t\n")
+                should_set_start_pos = True
+                male = female = 0
+                zero_male_genotype = 0
+                zero_one_female_genotype = zero_zero_female_genotype = 0
+            gender = "none"
+            genotype = "-1/-1"
+            for sample in samples:
+                genotype = record.genotype(sample)["GT"].lower().strip()
+                match_male_female = re.search(r"[\w]*_(\w)_[\d]+", sample)
+                if match_male_female:
+                    gender = match_male_female.group(1).lower().strip()
+                else:
+                    print("Not possible to find gender, aborting program")
+                    sys.exit(1)
+                male, female = accumulate_gender(male, female, gender, genotype)
+                zero_male_genotype, zero_one_female_genotype, zero_zero_female_genotype = accumulate_genotypes(zero_male_genotype, zero_one_female_genotype, zero_zero_female_genotype, genotype, gender)
+
     numerator = zero_male_genotype + zero_one_female_genotype + (2 * zero_zero_female_genotype)
     denominator = male + (2 * female)
     # (0/0 male GT + 0/1 female GT + (2 * 0/0 female GT)) / male + female
