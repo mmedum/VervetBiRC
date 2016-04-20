a77b3fea46a84ccab352dd9ac3f00e3159e87a79
Mark Medum Bundgaard <mmedum@gmail.com>
2016-04-20 21:19:48 +0200

    Created class for holding variables for each slice and updated calculation to do it in population groups

--- src/probability_of_zero/output_probability_in_slices.py
+++ src/probability_of_zero/output_probability_in_slices.py
@@ -5,9 +5,11 @@ for a specific X chromosome
 
 from os import path
 from argparse import ArgumentParser
+from collections import OrderedDict
 import vcf
 import re
 import sys
+import variable_holder
 
 def is_valid_file(parser, file):
     """Function for checking that a file exist"""
@@ -17,27 +19,62 @@ def is_valid_file(parser, file):
         return file
 
 
-def accumulate_gender(male, female, gender, genotype):
+def accumulate_gender(gender, genotype, variables):
     """Function for accumulating total he/she"""
     if gender == "m":
         if genotype == "0/0" or genotype == "1/1":
-            male += 1
+            variables.male += 1
     elif gender == "f":
         if genotype == "0/0" or genotype == "0/1" or genotype == "1/1":
-            female += 1
-    return male, female
+            variables.female += 1
 
 
-def accumulate_genotypes(zero_male_genotype, zero_one_female_genotype, zero_zero_female_genotype, genotype, gender):
+def accumulate_genotypes(gender, genotype, variables):
     """Function for accumulating genotype"""
     if gender == "m" and genotype == "0/0":
-        zero_male_genotype += 1
+        variables.zero_male_genotype += 1
     elif gender == "f":
         if genotype == "0/0":
-            zero_zero_female_genotype += 1
+            variables.zero_zero_female_genotype += 1
         elif genotype == "0/1":
-            zero_one_female_genotype += 1
-    return zero_male_genotype, zero_one_female_genotype, zero_zero_female_genotype
+            variables.zero_one_female_genotype += 1
+
+
+def setup_output_file(out, dict_population):
+    """Setup headers in output file"""
+    for key in dict_population:
+        string_key = str(key)
+        out.write(string_key)
+        out.write("\t")
+    out.write("\n")
+
+
+def setup_dict_population(dict_population):
+    """Setup dict for handling intermidiate results for each population group
+
+    10 population groups, where key 0 defines the total for all groups"""
+    dict_population["chromStart"] = ""
+    dict_population["chromEnd"] = ""
+    for i in range(11):
+        dict_population[i] = variable_holder.VariableHolder()
+
+
+def reset_dict_population(dict_population):
+    """Reset values in dict population"""
+    for key in range(11):
+        dict_population[key].reset_values()
+
+
+def output_dict_to_file(out, dict_population):
+    """Output the current information in dict to put file"""
+    out.write(dict_population["chromStart"])
+    out.write("\t")
+    out.write(dict_population["chromEnd"])
+    out.write("\t")
+    for key in range(11):
+        out.write(dict_population[key].probability_calculation())
+        out.write("\t")
+    out.write("\n")
 
 
 def main():
@@ -55,7 +92,10 @@ def main():
     args = parser.parse_args()
 
     with open(args.output, 'a') as out:
-        out.write("Start\tEnd\tProbability\n")
+        dict_population = OrderedDict()
+        setup_dict_population(dict_population)
+        setup_output_file(out, dict_population)
+
         vcf_reader = vcf.Reader(open(args.input, "r"))
         # Hold all the sample information
         samples = vcf_reader.samples
@@ -64,47 +104,39 @@ def main():
         should_set_start_pos = True
         start_pos = -1
 
-        male = female = 0
-        zero_male_genotype = 0
-        zero_one_female_genotype = zero_zero_female_genotype = 0
-
         for record in vcf_reader:
             if should_set_start_pos:
                 start_pos = record.POS
-                out.write(str(start_pos))
-                out.write("\t")
+                dict_population["chromStart"] = str(start_pos)
                 should_set_start_pos = False
             if (record.POS - start_pos) >= window_slice:
                 end_position = record.POS
-                out.write(str(end_position))
-                out.write("\t")
-                numerator = zero_male_genotype + zero_one_female_genotype + (2 * zero_zero_female_genotype)
-                denominator = male + (2 * female)
-                out.write(str(numerator/denominator))
-                out.write("\t\n")
-                should_set_start_pos = True
-                male = female = 0
-                zero_male_genotype = 0
-                zero_one_female_genotype = zero_zero_female_genotype = 0
+                dict_population["chromEnd"] = str(end_position)
+                output_dict_to_file(out, dict_population)
+                reset_dict_population(dict_population)
+                dict_population["chromStart"] = str(end_position)
             gender = "none"
+            population_group = -1
             genotype = "-1/-1"
+            current_variable_holder = None
             for sample in samples:
                 genotype = record.genotype(sample)["GT"].lower().strip()
-                match_male_female = re.search(r"[\w]*_(\w)_[\d]+", sample)
-                if match_male_female:
-                    gender = match_male_female.group(1).lower().strip()
+                match_population_gender = re.search(r"[\w]+_(\w)_(\d+)", sample)
+                if match_population_gender:
+                    gender = match_population_gender.group(1).lower().strip()
+                    population_group = int(match_population_gender.group(2).lower().strip())
+                    current_variable_holder = dict_population[population_group]
                 else:
-                    print("Not possible to find gender, aborting program")
+                    print("Not possible to find gender or population group, aborting program")
                     sys.exit(1)
-                male, female = accumulate_gender(male, female, gender, genotype)
-                zero_male_genotype, zero_one_female_genotype, zero_zero_female_genotype = accumulate_genotypes(zero_male_genotype, zero_one_female_genotype, zero_zero_female_genotype, genotype, gender)
+                accumulate_gender(gender, genotype, current_variable_holder)
+                accumulate_genotypes(gender, genotype, current_variable_holder)
+                # We need to do the total too
+                accumulate_gender(gender, genotype, dict_population[0])
+                accumulate_genotypes(gender, genotype, dict_population[0])
+        output_dict_to_file(out, dict_population)
 
-    numerator = zero_male_genotype + zero_one_female_genotype + (2 * zero_zero_female_genotype)
-    denominator = male + (2 * female)
-    # (0/0 male GT + 0/1 female GT + (2 * 0/0 female GT)) / male + female
-    print("Probability:", (numerator/denominator))
     print("Done")
-
 
 if __name__ == "__main__":
     main()
---
+++ src/probability_of_zero/variable_holder.py
@@ -0,0 +1,76 @@
+class VariableHolder(object):
+    """Calculation object for holding information about population group"""
+
+    def __init__(self):
+        self._male = 0
+        self._female = 0
+        self._zero_male_genotype = 0
+        self._zero_one_female_genotype = 0
+        self._zero_zero_female_genotype = 0
+
+    @property
+    def male(self):
+        """Get male"""
+        return self._male
+
+    @male.setter
+    def male(self, value):
+        """Set male"""
+        self._male = value
+
+    @property
+    def female(self):
+        """Get female"""
+        return self._female
+
+    @female.setter
+    def female(self, value):
+        """Set female"""
+        self._female = value
+
+    @property
+    def zero_male_genotype(self):
+        """Get male zero"""
+        return self._zero_male_genotype
+
+    @zero_male_genotype.setter
+    def zero_male_genotype(self, value):
+        """Set male zero"""
+        self._zero_male_genotype = value
+
+    @property
+    def zero_one_female_genotype(self):
+        """Get zero one female"""
+        return self._zero_one_female_genotype
+
+    @zero_one_female_genotype.setter
+    def zero_one_female_genotype(self, value):
+        """Set zero one female"""
+        self._zero_one_female_genotype = value
+
+    @property
+    def zero_zero_female_genotype(self):
+        """Get zero zero female"""
+        return self._zero_zero_female_genotype
+
+    @zero_zero_female_genotype.setter
+    def zero_zero_female_genotype(self, value):
+        """Set zero zero female"""
+        self._zero_zero_female_genotype = value
+
+    def reset_values(self):
+        """Reset all variables"""
+        self.male = 0
+        self.female = 0
+        self.zero_male_genotype = 0
+        self.zero_one_female_genotype = 0
+        self.zero_zero_female_genotype = 0
+
+    def probability_calculation(self):
+        """Perform Probability calculation based on own data"""
+#        print(self.male, self.female, self.zero_male_genotype, self.zero_one_female_genotype, self.zero_zero_female_genotype)
+        numerator = self.zero_male_genotype + self.zero_one_female_genotype + (2 * self.zero_zero_female_genotype)
+        denominator = self.male + (2 * self.female)
+#        print(self.male, self.female, self.zero_male_genotype, self.zero_one_female_genotype, self.zero_zero_female_genotype)
+        return str(numerator/denominator)
+
